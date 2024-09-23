ebfaf23a673eb67b02c434c979c8a86eda944c79
Adam Moore <redndahead@gmail.com>
2024-09-23 15:32:42 -0700

    Initial commit

---
+++ js/gsb-data-index-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * GSB Data Index Theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.gsbDataIndexTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
